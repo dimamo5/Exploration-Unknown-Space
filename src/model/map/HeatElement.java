ab1e27baa8d7cdf4481821f86fdcdeb2155e0cd6
dimamo5 <diogomoura13@gmail.com>
2016-12-07 03:18:41 +0000

    View Map

--- src/model/map/HeatElement.java
+++ src/model/map/HeatElement.java
@@ -24,16 +24,37 @@ public class HeatElement implements Drawable {
         y = ypos;
     }
 
+    public void addMyHeat() {
+        if (heat < 0) {
+            this.heat = 1;
+        } else {
+            this.heat++;
+        }
+    }
+
+    public void addOtherHeat() {
+        if (heat < 0 || heat != -2) {
+            heat = 0;
+        }
+    }
+    public void addVisionHeat() {
+        if (heat ==-1 ) {
+            heat = 0;
+        }
+    }
+    public void addWallHeat() {
+            heat = -2;
+    }
+
+
     @Override
     public void draw(SimGraphics g) {
-        if (heat > 0)
+        if (heat > 0)    //Numero de vezes que passou
             g.drawFastRect(new Color((heat > 9) ? 255 : heat * 25, 0, 0));
-        else if (heat == 0)
+        else if (heat == 0)  //passado pelos outros
             g.drawFastRect(new Color(10, 50, 100));
-        else if (heat == -2)
+        else if (heat == -2)  //Paredes
             g.drawFastRect(Color.gray);
-        else if (heat == -3)
-            g.drawFastRect(Color.YELLOW);
         else
             g.drawFastRect(Color.white);
     }
--- src/model/map/ViewMap.java
+++ src/model/map/ViewMap.java
@@ -1,5 +1,7 @@
 package model.map;
 
+import javafx.util.Pair;
+
 /**
  * Created by sergi on 05/12/2016.
  */
@@ -19,5 +21,64 @@ public class ViewMap {
 
     public HeatElement[][] getMap() {
         return map;
+    }
+
+    public void addViewRange(Pair<Integer, Integer> pos, Map map, int range) {
+        //Norte
+        for (int i = 0; i < range && i + pos.getKey() > 0; i++) {
+            if (map.getMap()[pos.getValue()][pos.getKey() + i] == 0 && i == 0) {
+                if (i == 0) {
+                    this.map[pos.getValue()][pos.getKey() + i].addMyHeat();
+                } else {
+                    this.map[pos.getValue()][pos.getKey() + i].addVisionHeat();
+                }
+            } else {
+                this.map[pos.getValue()][pos.getKey() + i].addWallHeat();
+                break;
+            }
+        }
+        //Sul
+        for (int i = 0; i < range && i + pos.getKey() > 0; i++) {
+            if (map.getMap()[pos.getValue()][pos.getKey() - i] == 0 && i == 0) {
+                if (i == 0) {
+                    this.map[pos.getValue()][pos.getKey() - i].addMyHeat();
+                } else {
+                    this.map[pos.getValue()][pos.getKey() - i].addVisionHeat();
+                }
+            } else {
+                this.map[pos.getValue()][pos.getKey() - i].addWallHeat();
+                break;
+            }
+        }
+        //Este
+        for (int i = 0; i < range && i + pos.getKey() > 0; i++) {
+            if (map.getMap()[pos.getValue() + i][pos.getKey()] == 0 && i == 0) {
+                if (i == 0) {
+                    this.map[pos.getValue() + i][pos.getKey()].addMyHeat();
+                } else {
+                    this.map[pos.getValue() + i][pos.getKey()].addVisionHeat();
+                }
+            } else {
+                this.map[pos.getValue() + i][pos.getKey()].addWallHeat();
+                break;
+            }
+        }
+        //Oeste
+        for (int i = 0; i < range && i + pos.getKey() > 0; i++) {
+            if (map.getMap()[pos.getValue() - i][pos.getKey()] == 0 && i == 0) {
+                if (i == 0) {
+                    this.map[pos.getValue() - i][pos.getKey()].addMyHeat();
+                } else {
+                    this.map[pos.getValue() - i][pos.getKey()].addVisionHeat();
+                }
+            } else {
+                this.map[pos.getValue() - i][pos.getKey()].addWallHeat();
+                break;
+            }
+        }
+    }
+
+    public void addViewMap(Map map) {
+        //TODO tomorrow
     }
 }

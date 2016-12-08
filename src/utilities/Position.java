5df4c5fd786f4bd0750f57ad4d2f8bccf8bb7ff5
dimamo5 <diogomoura13@gmail.com>
2016-12-08 22:16:29 +0000

    Position clas to be used later

---
+++ src/utilities/Position.java
@@ -0,0 +1,41 @@
+package utilities;
+
+/**
+ * Created by diogo on 08/12/2016.
+ */
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position)) return false;
+
+        Position position = (Position) o;
+
+        if (x != position.x) return false;
+        return y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+}
